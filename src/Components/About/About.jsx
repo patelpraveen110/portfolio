e67c43e24f579d24370a151dfe890c5ed5b9f2dc
patelpraveen110 <patelpraveen110@gmail.com>
2024-07-15 20:47:41 +0530

    working on the services component

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -8,28 +8,40 @@ const About = () => {
     <div className="about">
         <div className="about-title">
             <h1>About me</h1>
-            <img src={theme_pattern} alt="" />
+            {/* <img src={theme_pattern} alt="" /> */}
         </div>
         <div className="about-sections">
             <div className="about-left">
-                <img src={profile_imaage} alt="" />
+                <img src={profile_imaage} alt="" className='about-image'/>
             </div>
             <div className="about-right">
                 <div className="about-para">
-                    <p>details1 for portfolio user</p>
-                    <p>details2 for portfolio user</p> 
+                    <p>I am an experienced full stack developer with more than 3 years of profesional expertise in the field. Througout my career
+                        i have had the privilege of collaborating with prestigious organizations, contributing to their success and growth.
+                    </p>
+                    <p>I am an experienced full stack developer with more than 3 years of profesional expertise in the field. </p> 
                 </div>
                 <div className="about-skills">
-                    <div className="about-skill"><p>HTML $ CSS <hr style={{width:"50%"}}></hr></p>
-                    </div>
-                    <div className="about-skill"><p>React Js <hr style={{width:"60%"}}></hr></p>
-                    </div>
-                    <div className="about-skill"><p>Java<hr style={{width:"80%"}}></hr></p>
-                    </div>
-                    <div className="about-skill"><p>Spring Boot<hr style={{width:"70%"}}></hr></p>
-                    </div>
+                   <div className="about-skill"><p>Java </p><hr style={{width:"90%"}} /></div>
+                    <div className="about-skill"><p>React Js </p><hr style={{width:"60%"}} /></div>
+                    <div className="about-skill"><p>HTML & CSS </p> <hr style={{width:"50%"}} /></div>
+                    <div className="about-skill"><p>Spring Boot</p> <hr style={{width:"70%"}} /></div>
                 </div>
             </div>
+        </div>
+        <div className="about-achievements">
+            <div className="about-achievement">
+                <h1>3+</h1>
+                <p>YEARS OF EXPERIENCE</p>
+            </div><hr/>
+            <div className="about-achievement">
+                <h1>10+</h1>
+                <p>PROJECTS WORKED ON</p>
+            </div><hr/>
+            <div className="about-achievement">
+                <h1>3+</h1>
+                <p>HAPPY CLIENTS</p>
+            </div><hr/>
         </div>
     </div>
   )
---
+++ src/assets/services_data.js
@@ -0,0 +1,27 @@
+const Services_Data=[
+    {
+        s_no:"01",
+        s_name:"web design",
+        s_desc:"web development is the process of building, programming..."
+    },
+    {
+        s_no:"02",
+        s_name:"Graphics design",
+        s_desc:"web development is the process of building, programming..."
+    },
+    {
+        s_no:"03",
+        s_name:"Social design",
+        s_desc:"web development is the process of building, programming..."
+    },
+    {
+        s_no:"04",
+        s_name:"App design",
+        s_desc:"web development is the process of building, programming..."
+    },
+    {
+        s_no:"05    ",
+        s_name:"Android design",
+        s_desc:"web development is the process of building, programming..."
+    }
+]
