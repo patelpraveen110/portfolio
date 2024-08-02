a8a2ade9d8cee3cc77613f93235f84d92ae5ee79
patelpraveen110 <patelpraveen110@gmail.com>
2024-08-02 23:21:16 +0530

    updating project images

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,49 +1,69 @@
-import React from 'react'
-import './About.css'
-import theme_pattern from '../../assets/logo.png'
-import profile_imaage from '../../assets/profile.png'
+import React from "react";
+import "./About.css";
+import theme_pattern from "../../assets/logo.png";
+import profile_imaage from "../../assets/profile.png";
 
 const About = () => {
   return (
-    <div id='about' className="about">
-        <div className="about-title">
-            <h1>About Me</h1>
+    <div id="about" className="about">
+      <div className="about-title">
+        <h1>About Me</h1>
+      </div>
+      <div className="about-sections">
+        <div className="about-left">
+          <img src={profile_imaage} alt="" className="about-image" />
         </div>
-        <div className="about-sections">
-            <div className="about-left">
-                <img src={profile_imaage} alt="" className='about-image'/>
+        <div className="about-right">
+          <div className="about-para">
+            <p>
+              I am an experienced full stack developer with more than 3 years of
+              profesional expertise in the field. Througout my career i have had
+              the privilege of collaborating with prestigious organizations,
+              contributing to their success and growth.
+            </p>
+            <p>
+              My passion for frontend development is not only reflected in my
+              extensive experience but also in the enthusiasm and dedication I
+              bring to each project.
+            </p>
+          </div>
+          <div className="about-skills">
+            <div className="about-skill">
+              <p>Java </p>
+              <hr style={{ width: "90%" }} />
             </div>
-            <div className="about-right">
-                <div className="about-para">
-                    <p>I am an experienced full stack developer with more than 3 years of profesional expertise in the field. Througout my career
-                        i have had the privilege of collaborating with prestigious organizations, contributing to their success and growth.
-                    </p>
-                    <p>My passion for frontend development is not only reflected in my extensive experience but also in the enthusiasm and dedication I bring to each project.</p> 
-                </div>
-                <div className="about-skills">
-                   <div className="about-skill"><p>Java </p><hr style={{width:"90%"}} /></div>
-                    <div className="about-skill"><p>React Js </p><hr style={{width:"60%"}} /></div>
-                    <div className="about-skill"><p>HTML & CSS </p> <hr style={{width:"50%"}} /></div>
-                    <div className="about-skill"><p>Spring Boot</p> <hr style={{width:"70%"}} /></div>
-                </div>
+            <div className="about-skill">
+              <p>React Js </p>
+              <hr style={{ width: "60%" }} />
             </div>
+            <div className="about-skill">
+              <p>HTML & CSS </p> <hr style={{ width: "50%" }} />
+            </div>
+            <div className="about-skill">
+              <p>Spring Boot</p> <hr style={{ width: "70%" }} />
+            </div>
+          </div>
         </div>
-        <div className="about-achievements">
-            <div className="about-achievement">
-                <h1>3+</h1>
-                <p>YEARS OF EXPERIENCE</p>
-            </div><hr/>
-            <div className="about-achievement">
-                <h1>10+</h1>
-                <p>PROJECTS WORKED ON</p>
-            </div><hr/>
-            <div className="about-achievement">
-                <h1>3+</h1>
-                <p>HAPPY CLIENTS</p>
-            </div><hr/>
+      </div>
+      <div className="about-achievements">
+        <div className="about-achievement">
+          <h1>3+</h1>
+          <p>YEARS OF EXPERIENCE</p>
         </div>
+        <hr />
+        <div className="about-achievement">
+          <h1>10+</h1>
+          <p>TECHNOLOGIES WORKED ON</p>
+        </div>
+        <hr />
+        <div className="about-achievement">
+          <h1>3+</h1>
+          <p>PROJECTS WORKED ON</p>
+        </div>
+        <hr />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
